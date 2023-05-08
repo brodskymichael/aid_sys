4ed52f9f24800004095015a49639afa7c58d2750
milagros vetcher <milivetchergmail.com>
2023-05-08 17:34:29 -0300

    conect db

--- server/src/db.js
+++ server/src/db.js
@@ -5,7 +5,8 @@ import { getUsers } from './controllers/user.js';
 
 export async function dbConn(){
 
-    const url = 'mongodb://127.0.0.1:27017/spec'
+    //const url = 'mongodb://127.0.0.1:27017/spec'
+    const url = 'mongodb+srv://spec:12345@cluster0.npwwug2.mongodb.net/?retryWrites=true&w=majority'
 
    mongoose.connect(url,{
     })
