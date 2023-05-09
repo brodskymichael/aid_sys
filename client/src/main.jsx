1a0ea14a2c78c9b2d279d5ad532c99a2fff784ab
milagros vetcher <milivetchergmail.com>
2023-05-09 17:39:14 -0300

    cambio main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,10 +7,9 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from 'react-auth-kit';
-import axios from 'axios';
 //import refreshApi from "./refreshApi";
 //import { AuthProvider } from './context/AuthProvider';
-axios.defaults.baseURL = "https://spec-server.vercel.app/" ;
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
