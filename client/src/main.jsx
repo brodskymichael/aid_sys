3ba18c2067260a375d45796e268651136f9bd17d
milagros vetcher <milivetchergmail.com>
2023-05-09 17:12:53 -0300

    prueba url en frfont

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from 'react-auth-kit';
 //import refreshApi from "./refreshApi";
 //import { AuthProvider } from './context/AuthProvider';
-
+axios.defaults.baseURL = "https://vercel.com/milivetcher/spec-server/4TguCH49cVw6U3WrJFMuGAGKae4e" ;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
